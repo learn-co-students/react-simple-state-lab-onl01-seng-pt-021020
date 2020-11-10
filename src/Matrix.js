e17cc5b41cec3e1db1e048342c82432d12fd059a
amkera <60901491+amkera@users.noreply.github.com>
2020-11-09 21:22:55 -0500

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -4,14 +4,28 @@ import Cell from './Cell.js'
 export default class Matrix extends Component {
 
   genRow = (vals) => {
-    return vals.map(val => <Cell value={val} />)
+
+    return vals.map(val => <Cell value={val}/>) // replace me and render a cell component instead!
   }
-  //Once you have made your Cell component, replace the return value in genRow's map to: <Cell value={val} />.
-  //Here we are specifying our Cell component should have a value prop.
+  //vals = ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
 
   genMatrix = () => {
+    console.log(this.props.values)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
+
+//   this.props.values = [
+//     ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 1: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 2: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 3: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 4: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 5: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 6: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 7: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 8: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+// 9: (10) ["#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F", "#F00", "#00F"]
+//   ]
 
   render() {
     return (
@@ -29,5 +43,3 @@ Matrix.defaultProps = {
     return (new Array(10).fill(defRow))
   })()
 }
-
-//Make a default values prop for Matrix, which is a 10x10 array filled with the values '#F00' (red). For inspiration, take a look at src/data.js.
